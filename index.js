ede9e7b31b70170a9bbc5402a27508b2deb20f41
Matt Armstrong <matt@roofstock.com>
2016-01-19 14:59:55 -0800

    added missing file import.

--- index.js
+++ index.js
@@ -6,9 +6,9 @@ module.exports = {
 
   included: function(app) {
     this._super.included(app);
-
     app.import('vendor/Blob.js');
     app.import('bower_components/file-saver/FileSaver.js');
     app.import('bower_components/js-xlsx/dist/xlsx.js');
+    app.import('bower_components/js-xlsx/dist/jszip.js');
   }
 };
