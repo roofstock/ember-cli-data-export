31b88e43257b3b7d5db5b487c82ea6b5e8f1dfbc
Matt Armstrong <matt@roofstock.com>
2016-01-19 14:46:33 -0800

    more work on install

--- blueprints/ember-cli-data-export/index.js
+++ blueprints/ember-cli-data-export/index.js
@@ -1,10 +1,10 @@
 module.exports = {
-  description: 'Installs the underlying slider component using bower',
+  description: 'Installs the underlying xlsx component using bower',
 
   normalizeEntityName: function() {}, // no-op since we're just adding dependencies
 
 
-  afterInstall: function(options) {
+  afterInstall: function() {
      return this.addBowerPackagesToProject([
        { name: "file-saver" },
        { name: 'js-xlsx', target: '~0.8.0'}
