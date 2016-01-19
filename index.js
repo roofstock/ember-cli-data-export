8fe47a99e29dfee8e9eced23ebf6cbd689a9b47c
Matt Armstrong <matt@roofstock.com>
2016-01-19 15:08:05 -0800

    Needed to swap order of jszip and xlsx scripts for dependency reasons.

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ module.exports = {
     this._super.included(app);
     app.import('vendor/Blob.js');
     app.import('bower_components/file-saver/FileSaver.js');
-    app.import('bower_components/js-xlsx/dist/xlsx.js');
     app.import('bower_components/js-xlsx/dist/jszip.js');
+    app.import('bower_components/js-xlsx/dist/xlsx.js');
   }
 };
