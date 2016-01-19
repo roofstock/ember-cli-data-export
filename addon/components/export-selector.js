c8a32802357f3c6caa078bb0338efb96939839be
Matt Armstrong <matt@roofstock.com>
2016-01-18 21:19:33 -0800

    added selector component

---
+++ addon/components/export-selector.js
@@ -0,0 +1,14 @@
+import Ember from 'ember';
+import layout from '../templates/components/export-selector';
+
+export default Ember.Component.extend({
+  layout: layout,
+  types: ['Excel', 'CSV'],
+  selectedType: null,
+
+  actions: {
+    triggerExport: function() {
+      this.sendAction('exportData', selectedType);
+    }
+  }
+});
