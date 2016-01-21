bfe6fe521c978f03740bef92f322aad9e8d53b22
Matt Armstrong <matt@roofstock.com>
2016-01-21 12:24:51 -0800

    added component that triggers event onselect.

---
+++ addon/components/export-selector-onselect.js
@@ -0,0 +1,16 @@
+import Ember from 'ember';
+import layout from '../templates/components/export-selector-onselect';
+
+export default Ember.Component.extend({
+  layout: layout,
+  types: Ember.computed(function() {
+    return [this.get('selectPrompt'), 'Excel', 'CSV']
+  }),
+  selectPrompt: 'Export File',
+
+  actions: {
+    triggerExport: function() {
+      this.sendAction('exportData', this.get('selectedType'));
+    }
+  }
+});
