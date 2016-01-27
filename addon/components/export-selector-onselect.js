88455b87ff4b175736765418dbec9c867b249181
Matt Armstrong <matt@roofstock.com>
2016-01-27 12:03:14 -0800

    added code to flip the select box back to default state after change.

--- addon/components/export-selector-onselect.js
+++ addon/components/export-selector-onselect.js
@@ -8,6 +8,7 @@ export default Ember.Component.extend({
   actions: {
     triggerExport: function(selectedType) {
       this.sendAction('exportData', selectedType);
+      this.set('selectedType', '');
     }
   }
 });
