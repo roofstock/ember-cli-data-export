36c0441a02e9f26e5d9c3fb42f82f7b0f4ccb8c3
Gorzas <joseda87@gmail.com>
2017-05-29 17:41:37 +0200

    Separate jsonToCsv from export on csv service
    Related #17

--- addon/services/csv.js
+++ addon/services/csv.js
@@ -10,79 +10,76 @@ const defaultConfig = {
 export default Ember.Service.extend({
 
   export: function (data, options) {
-
     options = optionize(options, defaultConfig);
 
-    function JSON2CSV(objArray) {
-      var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+    var csv = this.jsonToCsv(data, options);
 
-      var str = '';
+    saveAs(new Blob([csv],{type:"data:text/csv;charset=utf-8"}), options.fileName);
+  },
+
+  jsonToCsv(objArray, options) {
+    var array = typeof objArray != 'object' ? JSON.parse(objArray) : objArray;
+
+    var str = '';
+    var line = '';
+
+    if (options.withSeparator) {
+      // add separator identifier;
+      str += `sep=${options.separator}\r\n`;
+    }
+
+    // add heading row
+    var head = array[0];
+    for (var i = 0; i < head.length; i++) {
+      var value = head[i] + "";
+      if (i > 0) {
+        line += options.separator;
+      }
+      line += '"' + value.replace(/"/g, '""') + '"';
+    }
+
+    str += line + '\r\n';
+
+    // add items
+    for (var i = 1; i < array.length; i++) {
       var line = '';
 
-      if (options.withSeparator) {
-        // add separator identifier;
-        str += `sep=${options.separator}\r\n`;
-      }
+      for (var index = 0; index < array[i].length; index++) {
+        var value = array[i][index];
 
-      // add heading row
-      var head = array[0];
-      for (var i = 0; i < head.length; i++) {
-        var value = head[i] + "";
-        if (i > 0) {
+        if (index > 0) {
           line += options.separator;
         }
-        line += '"' + value.replace(/"/g, '""') + '"';
+        if (typeof value === 'object') {
+          if (value) {
+            var resolveValue;
+            if (value._d instanceof Date) {
+              // dealing with encoding issue in IE browsers.
+              resolveValue = (value._d.getMonth() + 1) + '/' + value._d.getDate()  + '/' + value._d.getFullYear();
+            }
+            else {
+              resolveValue = value._d.toString();
+            }
+
+            line += '"' + resolveValue.replace(/"/g, '""') + '"';
+          }
+          else {
+            line += '""';
+          }
+        }
+        else {
+          value = value + "";
+          if (value && value != 'undefined') {
+            line += '"' + value.replace(/"/g, '""') + '"';
+          }
+          else {
+            line += '""';
+          }
+        }
       }
 
       str += line + '\r\n';
-
-      // add items
-      for (var i = 1; i < array.length; i++) {
-        var line = '';
-
-        for (var index = 0; index < array[i].length; index++) {
-          var value = array[i][index];
-
-          if (index > 0) {
-            line += options.separator;
-          }
-          if (typeof value === 'object') {
-            if (value) {
-              var resolveValue;
-              if (value._d instanceof Date) {
-                // dealing with encoding issue in IE browsers.
-                resolveValue = (value._d.getMonth() + 1) + '/' + value._d.getDate()  + '/' + value._d.getFullYear();
-              }
-              else {
-                resolveValue = value._d.toString();
-              }
-
-              line += '"' + resolveValue.replace(/"/g, '""') + '"';
-            }
-            else {
-              line += '""';
-            }
-          }
-          else {
-            value = value + "";
-            if (value && value != 'undefined') {
-              line += '"' + value.replace(/"/g, '""') + '"';
-            }
-            else {
-              line += '""';
-            }
-          }
-        }
-
-        str += line + '\r\n';
-      }
-      return str;
     }
-
-    var csv = JSON2CSV(data);
-
-    saveAs(new Blob([csv],{type:"data:text/csv;charset=utf-8"}), options.fileName);
-
+    return str;
   }
-
 });
