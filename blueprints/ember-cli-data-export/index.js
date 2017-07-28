bcdedb2d1d0dad93448aae4d10d999c877db32ea
osxi <osxi@users.noreply.github.com>
2017-07-28 18:43:01 -0500

    remove bower installs from default blueprint

--- blueprints/ember-cli-data-export/index.js
+++ blueprints/ember-cli-data-export/index.js
@@ -1,13 +1,5 @@
 module.exports = {
   description: 'Installs the underlying xlsx component using bower',
 
-  normalizeEntityName: function() {}, // no-op since we're just adding dependencies
-
-
-  afterInstall: function() {
-     return this.addBowerPackagesToProject([
-       { name: "file-saver" },
-       { name: 'js-xlsx', target: '~0.8.0'}
-     ]);
-   }
+  normalizeEntityName: function() {} // no-op since we're just adding dependencies
 };
