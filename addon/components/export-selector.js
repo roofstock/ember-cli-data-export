4de453b11fc6726a8bb3123c25d341f567fc5a77
osxi <osxi@users.noreply.github.com>
2017-07-28 16:25:39 -0500

    fix JSHint complaints

--- addon/components/export-selector.js
+++ addon/components/export-selector.js
@@ -4,7 +4,7 @@ import layout from '../templates/components/export-selector';
 export default Ember.Component.extend({
   layout: layout,
   types: Ember.computed(function() {
-    return [this.get('selectPrompt'), 'Excel', 'CSV']
+    return [this.get('selectPrompt'), 'Excel', 'CSV'];
   }),
   selectedType : null,
   actionText: 'Export',
--- addon/services/excel.js
+++ addon/services/excel.js
@@ -4,7 +4,7 @@ import optionize from "../utils/utils";
 const defaultConfig = {
   sheetName: 'Sheet1',
   fileName: 'export.xlsx'
-}
+};
 
 export default Ember.Service.extend({
 
@@ -15,46 +15,46 @@ export default Ember.Service.extend({
     function s2ab(s) {
       var buf = new ArrayBuffer(s.length);
       var view = new Uint8Array(buf);
-      for (var i=0; i!=s.length; ++i) view[i] = s.charCodeAt(i) & 0xFF;
+      for (var i=0; i!==s.length; ++i) { view[i] = s.charCodeAt(i) & 0xFF; }
       return buf;
     }
 
     function datenum(v, date1904) {
-      if(date1904) v+=1462;
+      if(date1904) { v+=1462; }
       var epoch = Date.parse(v);
       return (epoch - new Date(Date.UTC(1899, 11, 30))) / (24 * 60 * 60 * 1000);
     }
 
-    function sheet_from_array_of_arrays(data, opts) {
+    function sheet_from_array_of_arrays(data) {
       var ws = {};
       var range = {s: {c:10000000, r:10000000}, e: {c:0, r:0 }};
-      for(var R = 0; R != data.length; ++R) {
-        for(var C = 0; C != data[R].length; ++C) {
-          if(range.s.r > R) range.s.r = R;
-          if(range.s.c > C) range.s.c = C;
-          if(range.e.r < R) range.e.r = R;
-          if(range.e.c < C) range.e.c = C;
+      for(var R = 0; R !== data.length; ++R) {
+        for(var C = 0; C !== data[R].length; ++C) {
+          if(range.s.r > R) { range.s.r = R; }
+          if(range.s.c > C) { range.s.c = C; }
+          if(range.e.r < R) { range.e.r = R; }
+          if(range.e.c < C) { range.e.c = C; }
           var cell = {v: data[R][C] };
-          if(cell.v == null) continue;
+          if(cell.v == null) { continue; }
           var cell_ref = XLSX.utils.encode_cell({c:C,r:R});
 
-          if(typeof cell.v === 'number') cell.t = 'n';
-          else if(typeof cell.v === 'boolean') cell.t = 'b';
+          if(typeof cell.v === 'number') { cell.t = 'n'; }
+          else if(typeof cell.v === 'boolean') { cell.t = 'b'; }
           else if((typeof cell.v === 'object') && (cell.v._d instanceof Date)) {
             cell.t = 'n'; cell.z = XLSX.SSF._table[14];
             cell.v = datenum(cell.v._d);
           }
-          else cell.t = 's';
+          else { cell.t = 's'; }
 
           ws[cell_ref] = cell;
         }
       }
-      if(range.s.c < 10000000) ws['!ref'] = XLSX.utils.encode_range(range);
+      if(range.s.c < 10000000) { ws['!ref'] = XLSX.utils.encode_range(range); }
       return ws;
     }
 
     function Workbook() {
-      if(!(this instanceof Workbook)) return new Workbook();
+      if(!(this instanceof Workbook)) { return new Workbook(); }
       this.SheetNames = [];
       this.Sheets = {};
     }
