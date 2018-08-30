408c81cc8946d3f506c04d413dff7da64cce4b1c
Paul Nicholls <paul.n@linc-ed.com>
2018-08-30 15:11:32 +1200

    Run codemods

--- tests/unit/services/csv-test.js
+++ tests/unit/services/csv-test.js
@@ -1,28 +1,28 @@
-import { moduleFor, test } from 'ember-qunit';
+import { module, test } from 'qunit';
+import { setupTest } from 'ember-qunit';
 
-moduleFor('service:csv', 'Unit | Service | csv', {
-  // Specify the other units that are required for this test.
-  // needs: ['service:foo']
-});
+module('Unit | Service | csv', function(hooks) {
+  setupTest(hooks);
 
-// Replace this with your real tests.
-test('#jsonToCsv - with valid Array of Arrays, it should return an string with CSV format', function(assert) {
-  const service = this.subject();
-  const array = [
-    [
-      'name',
-      'last_name',
-    ],
-    [
-      'Dale',
-      'Cooper'
-    ],
-  ];
-  const expectedCSV = '\"name\",\"last_name\"\r\n\"Dale\",\"Cooper\"\r\n';
-  const options = {
-    separator: ',',
-    withSeparator: false,
-  };
+  // Replace this with your real tests.
+  test('#jsonToCsv - with valid Array of Arrays, it should return an string with CSV format', function(assert) {
+    const service = this.owner.lookup('service:csv');
+    const array = [
+      [
+        'name',
+        'last_name',
+      ],
+      [
+        'Dale',
+        'Cooper'
+      ],
+    ];
+    const expectedCSV = '\"name\",\"last_name\"\r\n\"Dale\",\"Cooper\"\r\n';
+    const options = {
+      separator: ',',
+      withSeparator: false,
+    };
 
-  assert.deepEqual(service.jsonToCsv(array, options), expectedCSV);
+    assert.deepEqual(service.jsonToCsv(array, options), expectedCSV);
+  });
 });
