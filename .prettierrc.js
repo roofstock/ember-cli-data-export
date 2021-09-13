ab8a7244f3e6ee05098340c1c7bb7be5e8e6082a
Charles Fries <charliefries@icloud.com>
2021-09-13 10:39:02 -0700

    Lint

--- .prettierrc.js
+++ .prettierrc.js
@@ -2,4 +2,13 @@
 
 module.exports = {
   singleQuote: true,
+  printWidth: 300,
+  overrides: [
+    {
+      files: '*.hbs',
+      options: {
+        singleQuote: false,
+      },
+    },
+  ],
 };
--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 'use strict';
 
 module.exports = {
-  name: 'ember-spreadsheet-export',
+  name: require('./package').name,
 
-  included: function(app) {
+  included: function (app) {
     this._super.included(app);
     app.import('vendor/Blob.js');
-  }
+  },
 };
